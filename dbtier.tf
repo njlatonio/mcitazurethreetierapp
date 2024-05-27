d977363f3b6d525a9446f7a730f0ec468757cbe3
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:43:13 -0400

    Update dbtier.tf

--- dbtier.tf
+++ dbtier.tf
@@ -12,3 +12,18 @@ resource "azurerm_network_security_group" "db_nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+# Network Security Rules
+resource "azurerm_network_security_rule" "db_allow_inbound" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                        = var.dbtier_ns_rules_name
+  priority                    = var.dbtier_ns_rules_priority
+  direction                   = var.dbtier_ns_rules_direction
+  access                      = var.dbtier_ns_rules_access
+  protocol                    = var.dbtier_ns_rules_protocol
+  source_port_range           = var.dbtier_ns_rules_source_port_range
+  destination_port_range      = var.dbtier_ns_rules_destination_port_range
+  source_address_prefix       = var.dbtier_ns_rules_source_address_prefix
+  destination_address_prefix  = var.dbtier_ns_rules_destination_address_prefix
+  network_security_group_name = azurerm_network_security_group.db_nsg.name
+}
