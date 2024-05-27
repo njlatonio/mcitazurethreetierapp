6b44582f5423ee594f3e9bf63a7d67acb4aa97f0
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:17:16 -0400

    Update apptier.tf

--- apptier.tf
+++ apptier.tf
@@ -5,3 +5,10 @@ resource "azurerm_subnet" "app" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.apptier_address_prefixes
 }
+
+# Network Security Groups
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = var.apptier_nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
