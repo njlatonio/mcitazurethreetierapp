a76e176ba59a1eb50391bc89d901a499687113b4
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:05:04 -0400

    Create virtualnetwork.tf

---
+++ virtualnetwork.tf
@@ -0,0 +1,7 @@
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
