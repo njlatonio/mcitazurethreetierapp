e1362c65da7f0228b6b9949f4e640ee21a00c4f7
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:07:10 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "Mcitterraformrg"
-  location = "Canada Central"
+  name     = var.resource_group_name
+  location = var.location
 }
