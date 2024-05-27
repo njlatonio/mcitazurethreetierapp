514f7c18007c5cdd54721fae43d4753d5f084f02
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:15:31 -0400

    Update webtier.tf

--- webtier.tf
+++ webtier.tf
@@ -5,3 +5,10 @@ resource "azurerm_subnet" "web" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.webtier_address_prefixes
 }
+
+# Network Security Groups
+resource "azurerm_network_security_group" "web_nsg" {
+  name                = var.webtier_nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
