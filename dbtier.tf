2cf3854b3cfa4617cad1d8fef69cc9f070aa1e8c
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:18:31 -0400

    Update dbtier.tf

--- dbtier.tf
+++ dbtier.tf
@@ -5,3 +5,10 @@
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.dbtier_address_prefixes
 } 
+
+# Network Security Groups
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = var.dbtier_nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
