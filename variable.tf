474566823a561e44d96ccc4457418209e85c4acb
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:25:21 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -44,3 +44,36 @@ variable "dbtier_address_prefixes"{
 variable "webtier_nsg_name"{
   type=string
 }
+variable "apptier_nsg_name"{
+  type=string
+}
+variable "dbtier_nsg_name"{
+  type=string
+}
+variable "webtier_ns_rules_name"{
+  type=string
+}
+variable "webtier_ns_rules_priority"{
+  type=number
+}
+variable "webtier_ns_rules_direction"{
+  type=number
+}
+variable "webtier_ns_rules_access"{
+  type=number
+}
+variable "webtier_ns_rules_protocol"{
+  type=number
+}
+variable "webtier_ns_rules_source_port_range"{
+  type=number
+}
+variable "webtier_ns_rules_destination_port_range"{
+  type=number
+}
+variable "webtier_ns_rules_source_address_prefix"{
+  type=number
+}
+variable "webtier_ns_rules_destination_address_prefix"{
+  type=number
+}
