0c482e621efba011907ae8aa791bad12d7d93895
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:37:55 -0400

    Update apptier.tf

--- apptier.tf
+++ apptier.tf
@@ -12,3 +12,18 @@ resource "azurerm_network_security_group" "app_nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+# Network Security Rules
+resource "azurerm_network_security_rule" "app_allow_inbound" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                        = var.apptier_ns_rules_name
+  priority                    = var.apptier_ns_rules_priority
+  direction                   = var.apptier_ns_rules_direction
+  access                      = var.apptier_ns_rules_access
+  protocol                    = var.apptier_ns_rules_protocol
+  source_port_range           = var.apptier_ns_rules_source_port_range
+  destination_port_range      = var.apptier_ns_rules_destination_port_range
+  source_address_prefix       = var.apptier_ns_rules_source_address_prefix
+  destination_address_prefix  = var.apptier_ns_rules_destination_address_prefix
+  network_security_group_name = azurerm_network_security_group.app_nsg.name
+}
