4fd87bb059518ea474184cfd4f3c10d435d95d46
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 19:08:42 -0400

    Update virtualnetwork.tf

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -1,7 +1,7 @@
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myVNet"
-  address_space       = ["10.0.0.0/16"]
+  name                = var.vnet_name
+  address_space       = var.address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
