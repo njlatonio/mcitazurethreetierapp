2b97cfafe48568195318a8acc10fc8e5a7e48f5d
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 20:18:02 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@
 output "web_vm_public_ip" {
   value = azurerm_linux_virtual_machine.web_vm.public_ip_address
 }
+
+#Output three tier module Web VM
+output "module_web_vm_public_ip" {
+  value = module.three_tier_app.module_web_vm_public_ip
+}
