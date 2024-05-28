0aa1f00fc94ec0396f430f8939d647661f0aa7f2
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-27 20:31:51 -0400

    Update main.tf

--- modulethreetierapp/main.tf
+++ modulethreetierapp/main.tf
@@ -223,7 +223,3 @@ resource "azurerm_mysql_database" "exampledb" {
   collation           = "utf8_general_ci"
 }
 
-# Output the Public IP of the Web VM
-output "web_vm_public_ip" {
-  value = azurerm_linux_virtual_machine.web_vm.public_ip_address
-}
